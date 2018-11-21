f474fd6420d2d2dca82411d9058d95cb080fb521
dinari amira <dinariamira@gmail.com>
2018-11-21 09:21:33 +0000

    Update heater_merge_all.py

--- heater_merge_all.py
+++ heater_merge_all.py
@@ -45,21 +45,25 @@ for file_ in filnames:
     df = pd.read_excel(file_, header=0)
     list_.append(df)
 heater_data_air_A = pd.concat(list_)
+
 def whole_merge(path):
     big_filenames=os.listdir(path)
+    list_big=[]
     for i in range(0,len(big_filenames)):
         p=path+'/'+big_filenames[i]
+        os.chdir(p)
         filnames=os.listdir(p)
         output = pd.DataFrame()
         list_ = []
         for file_ in filnames:
+           print(file_)
            df = pd.read_excel(file_, header=0)
            list_.append(df)
         output = pd.concat(list_)
-        if i==0:
-          dataframeone=output
-        else:
-          dataframe=pd.concat(dataframeone,output)
-          dataframeone=output
+        list_big.append(output)
+    dataframe=pd.concat(list_big,join="inner")
     return(dataframe)
+
+
+heater_data=whole_merge(path)  
 
